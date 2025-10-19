a80e48ca30ee51e6506ffe0e216462897372cd10
v4inztro <ferdhikahendriarto@gmail.com>
2025-10-19 14:11:59 +0700

    fix : token timeout ga berpengaruh di barang masuk dan barang keluar

--- views/distribution/barang_keluar.php
+++ views/distribution/barang_keluar.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+require_once __DIR__ . '/../../middleware/auth.php';
 include __DIR__ . '/../layouts/header.php';
 //include __DIR__ . '/../../controllers/DistribusiController.php';
 include __DIR__ . '/../layouts/sidebar.php';
